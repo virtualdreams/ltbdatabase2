08a47883bd8225e71369e728589f401db8b89750
Thomas Kindler <virtualdreamsdd@aol.com>
2014-04-11 08:01:01 +0200

    Updated json package.

--- ltbdb/Properties/AssemblyInfo.cs
+++ ltbdb/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern 
 // übernehmen, indem Sie "*" eingeben:
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.36.0")]
+[assembly: AssemblyFileVersion("1.0.37.0")]
