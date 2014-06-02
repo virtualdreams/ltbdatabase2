b65ce3b388198b211d79406497463661b300f615
Thomas Kindler <virtualdreamsdd@aol.com>
2014-06-02 11:03:40 +0200

    Added view by tag.

---
+++ ltbdb/Controllers/TagController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Castle.Core.Logging;
+using ltbdb.DomainServices;
+using ltbdb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ltbdb.Controllers
+{
+    public class TagController : Controller
+    {
+		public ILogger log { get; set; }
+
+        public ActionResult View(int? id)
+        {
+			var _tag = new Store().GetTag(id ?? 0);
+
+			var _books = _tag.GetBooks();
+
+			var tag = Mapper.Map<TagModel>(_tag);
+			var books = Mapper.Map<BookModel[]>(_books);
+
+			var view = new BookViewTagModel { Books = books, Tag = tag };
+
+			return View(view);
+        }
+    }
+}
---
+++ ltbdb/Models/ViewModels/BookViewTagModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ltbdb.Models
+{
+	public class BookViewTagModel
+	{
+		public BookModel[] Books { get; set; }
+		public TagModel Tag { get; set; }
+	}
+}
