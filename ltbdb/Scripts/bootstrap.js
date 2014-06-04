dc7ea6bc511e0958e3caa6e39ac158f0fcf8307e
Thomas Kindler <virtualdreamsdd@aol.com>
2014-06-04 14:09:07 +0200

    Added jquery and tooltips for tag ref count.

---
+++ ltbdb/Scripts/bootstrap.js
@@ -0,0 +1,26 @@
+$.widget("custom.catcomplete", $.ui.autocomplete, {
+	_renderMenu: function(ul, items) {
+		var that = this, currentCategory = "";
+		$.each(items, function(index, item) {
+			if(item.category != currentCategory) {
+				ul.append('<li class="ui-autocomplete-category">' + item.category + '</li>');
+				currentCategory = item.category;
+			}
+			that._renderItemData(ul, item);
+		});
+	}
+});
+
+$(function() {
+	$('.tt').tooltip({
+		track: true
+	});
+	
+	$(document).on('click', '.message-hidden-field', function() {
+		$(this).remove();
+	});
+	
+	$(document).on('mouseover', '.message-hidden-field', function() {
+		$(this).tooltip();
+	});
+});
