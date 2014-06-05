267caa1a4df4d977516ddf374208c70064c57d52
Thomas Kindler <virtualdreamsdd@aol.com>
2014-06-05 10:34:40 +0200

    Changed version number.

--- ltbdb/Properties/AssemblyInfo.cs
+++ ltbdb/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern 
 // übernehmen, indem Sie "*" eingeben:
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.123.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: AssemblyInformationalVersion("0cef6a9")]
+[assembly: AssemblyInformationalVersion("f06eb61-dev")]
