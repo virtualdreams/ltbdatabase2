f648756ae8ce5f29c5f5bd6e175d278b97abc991
Thomas Kindler <virtualdreamsdd@aol.com>
2014-07-16 08:26:13 +0200

    Extended config file reader.

--- ltbdb/Core/Helpers/ConfigFile.cs
+++ ltbdb/Core/Helpers/ConfigFile.cs
@@ -8,7 +8,45 @@ using System.Text;
 namespace CS.Helper
 {
 	/// <summary>
-	/// Class to read simple line based config key-value pairs
+	/// Class to read simple line based config key-value pairs.
+	/// 
+	/// Lines beginnign with '#' or ';' are treated as comments. Empty
+	/// lines are ignored.
+	/// Line contains a '=' are treated as a key = value - pair. Values can
+	/// continued by placing a leading backslash at the of the line. Comment
+	/// lines may appear in the middle of a sequence of continuation lines.
+	/// 
+	/// Trailing and leading whitespaces are ignored in each line, key and value.
+	/// 
+	/// If you want to preserve leading or trailing whitespaces of a value,
+	/// sourround it with double quotes ('"').
+	/// 
+	/// Examples:
+	/// 
+	/// # Standard 
+	/// key1 = value1
+	/// key2 = " value2 "
+	/// 
+	/// # Multiline value
+	/// key3 =	value3\
+	///			value4\
+	///	# One more value
+	///			value5
+	///		
+	/// # This is ignored
+	/// ;key4 = value6
+	/// 
+	/// # The leading and trailing whitespace are preseverd
+	/// key5 =	" value7, value8,\
+	///			value9, value10\
+	///			value11 "
+	///			
+	/// # This is valid, but the value is null
+	/// key6 = 
+	/// 
+	/// # The following two samples are ignored if not in continuation mode
+	///  = value12
+	/// some line without
 	/// </summary>
 	public class ConfigFile
 	{
@@ -28,37 +66,84 @@ namespace CS.Helper
 		}
 
 		/// <summary>
-		/// Open config file and read it.
+		/// Open config file and read in.
 		/// </summary>
 		/// <param name="filename"></param>
 		public void Read(string filename)
 		{
-			if (File.Exists(filename))
-			{
-				_configValues.Clear();
-				string[] lines = File.ReadAllLines(filename);
+			if (!File.Exists(filename))
+				return;
 
-				foreach (string line in lines)
+			_configValues.Clear();
+
+			string[] lines = File.ReadAllLines(filename);
+
+			string key = "";
+			string value = "";
+			bool continuation = false;
+
+			foreach (string line in lines)
+			{
+				string c = line.Trim();
+
+				// Test for comments or empty lines
+				if (c.StartsWith("#") || c.StartsWith(";") || c.Length == 0)
 				{
-					string c = line.Trim();
-					
-					if (c.StartsWith("#") || c.StartsWith(";") || c.Length == 0)
+					continue;
+				}
+
+				// Split to <key> = <value> pair
+				string[] pair = c.Split(new char[] { '=' }, 2);
+
+				// Test for one part and not in continuation mode
+				if (pair.Length == 1 && !continuation)
+				{
+					continue;
+				}
+
+				// Test for two parts not in continuation mode
+				if (pair.Length == 2 && !continuation)
+				{
+					key = pair[0].Trim();
+					value = pair[1].Trim();
+
+					if (key.Length == 0)
 					{
 						continue;
 					}
-
-					string[] pair = c.Split(new char[] {'='}, 2);
-					if (pair.Length == 2)
-					{
-						string key = pair[0].Trim();
-						string value = pair[1].Trim();
-
-						if (!_configValues.Contains(key))
-						{
-							_configValues.Add(key, value);
-						}
-					}
 				}
+
+				// Test for continuation mode
+				if (continuation)
+				{
+					value = String.Concat(value, c);
+				}
+
+				// Test for trailing backslash
+				if (value.EndsWith("\\"))
+				{
+					value = value.Substring(0, value.Length - 1);
+					continuation = true;
+					continue;
+				}
+
+				// Test for surrounding double quotes
+				if (value.StartsWith("\"") && value.EndsWith("\""))
+				{
+					value = value.Substring(1, value.Length - 2);
+				}
+				
+				// Add the <key> = <value> to store or override
+				if (!_configValues.Contains(key))
+				{
+					_configValues.Add(key, value);
+				}
+				else
+				{
+					_configValues[key] = value;
+				}
+
+				continuation = false;
 			}
 		}
 
