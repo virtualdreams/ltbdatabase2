62c06575f3cb48522b28403ea2a9fc1cb405284e
Thomas Kindler <virtualdreamsdd@aol.com>
2014-07-25 07:59:57 +0200

    Updated ConfigFile

--- ltbdb/Core/Helpers/ConfigFile.cs
+++ ltbdb/Core/Helpers/ConfigFile.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CS.Helper
 {
@@ -20,6 +22,8 @@ namespace CS.Helper
 	/// 
 	/// If you want to preserve leading or trailing whitespaces of a value,
 	/// sourround it with double quotes ('"').
+	/// 
+	/// The value can contain a '\n' for a newline. You can escape this with '\\n'.
 	/// 
 	/// Examples:
 	/// 
@@ -78,12 +82,15 @@ namespace CS.Helper
 
 			string[] lines = File.ReadAllLines(filename);
 
+			int fline = 0;
 			string key = "";
 			string value = "";
 			bool continuation = false;
 
 			foreach (string line in lines)
 			{
+				fline++;
+
 				string c = line.Trim();
 
 				// Test for comments or empty lines
@@ -98,7 +105,9 @@ namespace CS.Helper
 				// Test for one part and not in continuation mode
 				if (pair.Length == 1 && !continuation)
 				{
-					continue;
+					// error
+					throw new ConfigFileException(String.Format("No key value-pair found on line {0}.", fline));
+					//continue;
 				}
 
 				// Test for two parts not in continuation mode
@@ -109,7 +118,9 @@ namespace CS.Helper
 
 					if (key.Length == 0)
 					{
-						continue;
+						// error
+						throw new ConfigFileException(String.Format("Key can't zero length on line {0}.", fline));
+						//continue;
 					}
 				}
 
@@ -133,6 +144,10 @@ namespace CS.Helper
 					value = value.Substring(1, value.Length - 2);
 				}
 				
+				// escape newline and replace escaped newline
+				value = Regex.Replace(value, @"((?<!\\)\\n)", Environment.NewLine);
+				value = Regex.Replace(value, @"(\\\\)(n)", "\\$2");
+
 				// Add the <key> = <value> to store or override
 				if (!_configValues.Contains(key))
 				{
@@ -170,6 +185,56 @@ namespace CS.Helper
 				return _configValues[key] as string;
 			}
 			return defaultValue;
+		}
+	}
+
+	/// <summary>
+	/// This exception is thrown when an error in the ConfigFile occurs.
+	/// </summary>
+	/// <remarks>
+	/// This is the base exception for all exceptions thrown in the SVweb
+	/// </remarks>
+	[Serializable]
+	public class ConfigFileException : ApplicationException
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:CS.Helper.ConfigFileException" /> class.
+		/// </summary>
+		public ConfigFileException()
+			: base("ConfigFile caused an exception.")
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:CS.Helper.ConfigFileException" /> class.
+		/// </summary>
+		public ConfigFileException(Exception ex)
+			: base("ConfigFile caused an exception.", ex)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:CS.Helper.ConfigFileException" /> class.
+		/// </summary>
+		public ConfigFileException(string message)
+			: base(message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:CS.Helper.ConfigFileException" /> class.
+		/// </summary>
+		public ConfigFileException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:CS.Helper.ConfigFileException" /> class.
+		/// </summary>
+		protected ConfigFileException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
 		}
 	}
 }
