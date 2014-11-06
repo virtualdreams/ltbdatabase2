a2ab5bac6430821c500ee7e94940f6bbcbbc8922
Thomas Kindler <virtualdreamsdd@aol.com>
2014-11-06 15:22:48 +0100

    Updated schema creation.

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,107 +1,41 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE DATABASE `ltbdb2` DEFAULT CHARACTER SET utf8;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema ltbdb2
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ltbdb2` DEFAULT CHARACTER SET utf8 ;
-USE `ltbdb2` ;
+USE `ltbdb2`;
 
--- -----------------------------------------------------
--- Table `ltbdb2`.`ltbdb_categories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ltbdb2`.`ltbdb_categories` (
-  `catid` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` TEXT NOT NULL,
-  PRIMARY KEY (`catid`),
-  INDEX `TEXT` USING BTREE (`name`(20) ASC))
-ENGINE = InnoDB
-AUTO_INCREMENT = 17
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `ltbdb2`.`ltbdb_books`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ltbdb2`.`ltbdb_books` (
-  `bookid` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` TEXT NOT NULL,
-  `number` INT(11) NOT NULL,
-  `catid` INT(11) NOT NULL,
-  `image` TEXT NULL DEFAULT NULL,
-  `added` DATETIME NOT NULL,
+CREATE TABLE `books` (
+  `bookid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `number` int(11) NOT NULL,
+  `catid` int(11) NOT NULL,
+  `image` text,
+  `added` datetime NOT NULL,
+  `stories` text,
   PRIMARY KEY (`bookid`),
-  INDEX `TEXT` USING BTREE (`name`(20) ASC),
-  INDEX `fk_cat` (`catid` ASC),
-  CONSTRAINT `fk_cat`
-    FOREIGN KEY (`catid`)
-    REFERENCES `ltbdb2`.`ltbdb_categories` (`catid`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 377
-DEFAULT CHARACTER SET = utf8;
+  KEY `TEXT` (`name`(20)) USING BTREE,
+  KEY `fk_cat` (`catid`),
+  CONSTRAINT `fk_cat` FOREIGN KEY (`catid`) REFERENCES `categories` (`catid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `categories` (
+  `catid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  PRIMARY KEY (`catid`),
+  KEY `TEXT` (`name`(20)) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- -----------------------------------------------------
--- Table `ltbdb2`.`ltbdb_stories`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ltbdb2`.`ltbdb_stories` (
-  `storyid` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` TEXT NOT NULL,
-  `bookid` INT(11) NOT NULL,
-  PRIMARY KEY (`storyid`),
-  INDEX `TEXT` USING BTREE (`name`(20) ASC),
-  INDEX `fk_book` (`bookid` ASC),
-  CONSTRAINT `fk_book`
-    FOREIGN KEY (`bookid`)
-    REFERENCES `ltbdb2`.`ltbdb_books` (`bookid`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 84
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `ltbdb2`.`ltbdb_tags`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ltbdb2`.`ltbdb_tags` (
-  `tagid` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` TEXT NOT NULL,
-  PRIMARY KEY (`tagid`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-
--- -----------------------------------------------------
--- Table `ltbdb2`.`ltbdb_tag2book`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ltbdb2`.`ltbdb_tag2book` (
-  `tag2book` INT(11) NOT NULL AUTO_INCREMENT,
-  `tagid` INT(11) NOT NULL,
-  `bookid` INT(11) NOT NULL,
+CREATE TABLE `tag2book` (
+  `tag2book` int(11) NOT NULL AUTO_INCREMENT,
+  `tagid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL,
   PRIMARY KEY (`tag2book`),
-  INDEX `fk_tag_idx` (`tagid` ASC),
-  INDEX `fk_book_idx` (`bookid` ASC),
-  CONSTRAINT `fk_book2`
-    FOREIGN KEY (`bookid`)
-    REFERENCES `ltbdb2`.`ltbdb_books` (`bookid`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_tag`
-    FOREIGN KEY (`tagid`)
-    REFERENCES `ltbdb2`.`ltbdb_tags` (`tagid`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  KEY `fk_tag_idx` (`tagid`),
+  KEY `fk_book_idx` (`bookid`),
+  CONSTRAINT `fk_book2` FOREIGN KEY (`bookid`) REFERENCES `books` (`bookid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_tag` FOREIGN KEY (`tagid`) REFERENCES `tags` (`tagid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+CREATE TABLE `tags` (
+  `tagid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  PRIMARY KEY (`tagid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
