d575685287575ef3cebdd3b84d1959e5148e92ad
Thomas Kindler <virtualdreamsdd@aol.com>
2016-04-21 10:55:24 +0200

    Updated version information class.

--- ltbdb/Core/VersionInfo.cs
+++ ltbdb/Core/VersionInfo.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace ltbdb.Core
 {
 	static public class VersionInfo
 	{
+		/// <summary>
+		/// Get the file version.
+		/// </summary>
 		public static string FileVersion
 		{
 			get
 			{
 				Assembly asm = Assembly.GetExecutingAssembly();
 				FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(asm.Location);
-				
+
 				return fvi.FileVersion;
 			}
 		}
 
+		/// <summary>
+		/// Get the product version.
+		/// </summary>
 		public static string ProductVersion
 		{
 			get
@@ -28,9 +35,24 @@ namespace ltbdb.Core
 				FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(asm.Location);
 
 				return fvi.ProductVersion;
-				
-				//var build = ((AssemblyInformationalVersionAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false)[0]).InformationalVersion;
-				//return build;
+			}
+		}
+
+		/// <summary>
+		/// Get the git hash if available.
+		/// </summary>
+		public static string GitHash
+		{
+			get
+			{
+				Assembly asm = Assembly.GetExecutingAssembly();
+				FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(asm.Location);
+
+				var match = Regex.Match(fvi.ProductVersion, "([0-9a-f]{7,7})", RegexOptions.IgnoreCase);
+				if (match.Success)
+					return match.Groups[1].Value;
+				else
+					return "";
 			}
 		}
 	}
