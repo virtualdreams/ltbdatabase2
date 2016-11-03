a043e1ab7b4fa81447e673cca2e347faad1472a4
Thomas Kindler <virtualdreamsdd@aol.com>
2016-11-03 16:58:26 +0100

    Updated ToSlug() comments.

--- ltbdb/Core/Helpers/StringExtensions.cs
+++ ltbdb/Core/Helpers/StringExtensions.cs
@@ -16,23 +16,23 @@ namespace ltbdb.Core.Helpers
 		/// <returns>Slugified string.</returns>
 		static public string ToSlug(this string value)
 		{
-			//First to lower case
+			// convert to lower case
 			value = value.ToLowerInvariant();
 
-			//Remove all accents
+			// remove all accents
 			var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(value);
 			value = Encoding.ASCII.GetString(bytes);
 
-			//Replace spaces
+			// replace spaces
 			value = Regex.Replace(value, @"\s", "-", RegexOptions.Compiled);
 
-			//Remove invalid chars
+			// remove invalid chars
 			value = Regex.Replace(value, @"[^a-z0-9\s-_]", "", RegexOptions.Compiled);
 
-			//Trim dashes from end
+			// trim dashes from end
 			value = value.Trim('-', '_');
 
-			//Replace double occurences of - or _
+			// replace double occurences of - or _
 			value = Regex.Replace(value, @"([-_]){2,}", "$1", RegexOptions.Compiled);
 
 			return value;
